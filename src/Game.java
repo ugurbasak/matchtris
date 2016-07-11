d677fb01601a55f100344805b65efa2cee24e2b3
Ugur Basak <ugurbasak@gmail.com>
2016-07-11 21:03:48 +0200

    Moved store (saving and loading) related codes to different class

--- src/Game.java
+++ src/Game.java
@@ -120,6 +120,7 @@ public class Game extends JPanel implements Runnable {
     public static int GameMode = 2;
     private Board board = null;
     private Menu menu = null;
+    private Store store = null;
     public MatchStone matchstone = null;
 
     private long lastDraw;
@@ -150,44 +151,12 @@ public class Game extends JPanel implements Runnable {
         font = new Font("TimesRoman", Font.PLAIN, 20);
         board = new Board();
         menu = new Menu();
+        store = new Store();
         //Initialization
         HighScores = new int[10];
         lastDraw = System.currentTimeMillis();
+        store.loadScores();
 
-        /*
-        Loading and saving game can be impelemented later   
-                try {
-                    //about record store
-                    byte[] dummy = "0".getBytes();
-                    rsMatchTris = RecordStore.openRecordStore("MatchTris",true);
-                    if(rsMatchTris.getNumRecords() != NoOfRecords){
-                        rsMatchTris.closeRecordStore();
-                        RecordStore.deleteRecordStore("MatchTris");
-                        rsMatchTris = RecordStore.openRecordStore("MatchTris",true);
-                        for(int i=0; i<NoOfRecords; i++)
-                            rsMatchTris.addRecord(dummy,0,dummy.length);
-                        System.out.println("Added");
-                    }
-                    else{
-                        //load i?lemlerini yap burada
-                        Level = GetIntRecord(rsMatchTris,LEVEL);
-                        for(int i=0; i<10; i++)
-                            HighScores[i] = GetIntRecord(rsMatchTris,HIGHSCORES+i);
-                        System.out.println("Level "+Level);
-                        for(int i=0; i<10; i++)
-                            System.out.println("HighScores "+HighScores[i]);
-                        Continue = GetIntRecord(rsMatchTris,ISCONTINUE);
-                    }
-                    System.out.println("You have "+rsMatchTris.getNumRecords()+" records");
-                    rsMatchTris.closeRecordStore();
-                } catch (RecordStoreFullException e) {
-                    e.printStackTrace();
-                } catch (RecordStoreNotFoundException e) {
-                    e.printStackTrace();
-                } catch (RecordStoreException e) {
-                    e.printStackTrace();
-                } */
-        //}
 
 
         //if(dg==null) {
@@ -382,7 +351,8 @@ public class Game extends JPanel implements Runnable {
                                         System.out.println("Starts");
                                         NewGame();
                                     } else if (menuValue == 1) { //CONTINUE
-                                        SaveLoad(false);
+                                        matchstone = new MatchStone(); //UBASAK, why do we need to create???
+                                        store.SaveLoad(false);
                                         System.out.println("Loaded");
                                         GAMEOVER = false;
                                         GameMode = 0;
@@ -508,83 +478,5 @@ public class Game extends JPanel implements Runnable {
         }
         matchstone.CellY = 0;
         transCell = 0;
-    }
-
-    /*  
-    private int GetIntRecord(RecordStore rs, int no){
-        int x=0;
-        try {
-            byte[] temp = rs.getRecord(no);
-            String temp_str = new String(temp,0,temp.length);
-            x=Integer.parseInt(temp_str);
-        } catch (RecordStoreNotOpenException e) {
-            e.printStackTrace();
-        } catch (InvalidRecordIDException e) {
-            e.printStackTrace();
-        } catch (RecordStoreException e) {
-            e.printStackTrace();
-        }
-        return x;
-    }
-    
-    private void SetRecord(RecordStore rs,int no,int var){
-        byte[] temp =( (String)(""+var) ).getBytes();
-        try {
-            rs.setRecord(no,temp,0,temp.length);
-        } catch (RecordStoreNotOpenException e) {
-            e.printStackTrace();
-        } catch (InvalidRecordIDException e) {
-            e.printStackTrace();
-        } catch (RecordStoreFullException e) {
-            e.printStackTrace();
-        } catch (RecordStoreException e) {
-            e.printStackTrace();
-        }
-    }*/
-
-    
-
-    
-
-    public void SaveLoad(boolean Save) {
-        if (!Save) matchstone = new MatchStone();
-        long timex = System.currentTimeMillis();
-        /*
-        try {
-            rsMatchTris = RecordStore.openRecordStore("MatchTris",true);
-            for(int i=0; i<200; i++){
-                if(Save)
-                    SetRecord(rsMatchTris,LOADTHEM+i,TetrisBoard[i/20][i%20]);
-                else
-                    TetrisBoard[i/20][i%20] = GetIntRecord(rsMatchTris,LOADTHEM+i);
-            }
-            if(Save) SetRecord(rsMatchTris,ISCONTINUE,1);
-            for(int i=0; i<3; i++){
-                if(Save)
-                    SetRecord(rsMatchTris,213+i,matchstone.type[i]);
-                else
-                    matchstone.type[i]= GetIntRecord(rsMatchTris,213+i);
-            }
-            if(Save){
-                SetRecord(rsMatchTris,216,matchstone.CellX);
-                SetRecord(rsMatchTris,217,matchstone.CellY);
-                SetRecord(rsMatchTris,218,puan);
-            }
-            else{
-                matchstone.CellX = GetIntRecord(rsMatchTris,216);
-                matchstone.CellY = GetIntRecord(rsMatchTris,217);
-                puan = GetIntRecord(rsMatchTris,218);
-                SetRecord(rsMatchTris,ISCONTINUE,0);
-            }
-            rsMatchTris.closeRecordStore();
-        } catch (RecordStoreFullException e) {
-            e.printStackTrace();
-        } catch (RecordStoreNotFoundException e) {
-            e.printStackTrace();
-        } catch (RecordStoreException e) {
-            e.printStackTrace();
-        } */
-        timex = System.currentTimeMillis() - timex;
-        System.out.println("timepassed in ms " + timex + " in sec " + (timex / 1000));
     }
 }
---
+++ src/Store.java
@@ -0,0 +1,120 @@
+public class Store {
+
+    public Store() {
+
+    }
+
+    public void loadScores() {
+    	        /*
+        Loading and saving game can be impelemented later   
+                try {
+                    //about record store
+                    byte[] dummy = "0".getBytes();
+                    rsMatchTris = RecordStore.openRecordStore("MatchTris",true);
+                    if(rsMatchTris.getNumRecords() != NoOfRecords){
+                        rsMatchTris.closeRecordStore();
+                        RecordStore.deleteRecordStore("MatchTris");
+                        rsMatchTris = RecordStore.openRecordStore("MatchTris",true);
+                        for(int i=0; i<NoOfRecords; i++)
+                            rsMatchTris.addRecord(dummy,0,dummy.length);
+                        System.out.println("Added");
+                    }
+                    else{
+                        //load i?lemlerini yap burada
+                        Level = GetIntRecord(rsMatchTris,LEVEL);
+                        for(int i=0; i<10; i++)
+                            HighScores[i] = GetIntRecord(rsMatchTris,HIGHSCORES+i);
+                        System.out.println("Level "+Level);
+                        for(int i=0; i<10; i++)
+                            System.out.println("HighScores "+HighScores[i]);
+                        Continue = GetIntRecord(rsMatchTris,ISCONTINUE);
+                    }
+                    System.out.println("You have "+rsMatchTris.getNumRecords()+" records");
+                    rsMatchTris.closeRecordStore();
+                } catch (RecordStoreFullException e) {
+                    e.printStackTrace();
+                } catch (RecordStoreNotFoundException e) {
+                    e.printStackTrace();
+                } catch (RecordStoreException e) {
+                    e.printStackTrace();
+                } */
+        //}
+    }
+
+    /*  
+    private int GetIntRecord(RecordStore rs, int no){
+        int x=0;
+        try {
+            byte[] temp = rs.getRecord(no);
+            String temp_str = new String(temp,0,temp.length);
+            x=Integer.parseInt(temp_str);
+        } catch (RecordStoreNotOpenException e) {
+            e.printStackTrace();
+        } catch (InvalidRecordIDException e) {
+            e.printStackTrace();
+        } catch (RecordStoreException e) {
+            e.printStackTrace();
+        }
+        return x;
+    }
+    
+    private void SetRecord(RecordStore rs,int no,int var){
+        byte[] temp =( (String)(""+var) ).getBytes();
+        try {
+            rs.setRecord(no,temp,0,temp.length);
+        } catch (RecordStoreNotOpenException e) {
+            e.printStackTrace();
+        } catch (InvalidRecordIDException e) {
+            e.printStackTrace();
+        } catch (RecordStoreFullException e) {
+            e.printStackTrace();
+        } catch (RecordStoreException e) {
+            e.printStackTrace();
+        }
+    }*/
+
+    
+
+    
+
+    public void SaveLoad(boolean Save) {
+        long timex = System.currentTimeMillis();
+        /*
+        try {
+            rsMatchTris = RecordStore.openRecordStore("MatchTris",true);
+            for(int i=0; i<200; i++){
+                if(Save)
+                    SetRecord(rsMatchTris,LOADTHEM+i,TetrisBoard[i/20][i%20]);
+                else
+                    TetrisBoard[i/20][i%20] = GetIntRecord(rsMatchTris,LOADTHEM+i);
+            }
+            if(Save) SetRecord(rsMatchTris,ISCONTINUE,1);
+            for(int i=0; i<3; i++){
+                if(Save)
+                    SetRecord(rsMatchTris,213+i,matchstone.type[i]);
+                else
+                    matchstone.type[i]= GetIntRecord(rsMatchTris,213+i);
+            }
+            if(Save){
+                SetRecord(rsMatchTris,216,matchstone.CellX);
+                SetRecord(rsMatchTris,217,matchstone.CellY);
+                SetRecord(rsMatchTris,218,puan);
+            }
+            else{
+                matchstone.CellX = GetIntRecord(rsMatchTris,216);
+                matchstone.CellY = GetIntRecord(rsMatchTris,217);
+                puan = GetIntRecord(rsMatchTris,218);
+                SetRecord(rsMatchTris,ISCONTINUE,0);
+            }
+            rsMatchTris.closeRecordStore();
+        } catch (RecordStoreFullException e) {
+            e.printStackTrace();
+        } catch (RecordStoreNotFoundException e) {
+            e.printStackTrace();
+        } catch (RecordStoreException e) {
+            e.printStackTrace();
+        } */
+        timex = System.currentTimeMillis() - timex;
+        System.out.println("timepassed in ms " + timex + " in sec " + (timex / 1000));
+    }
+}
